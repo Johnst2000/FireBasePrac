f3c5013a32ed6343142ea155b08a672e45c5148c
JOHN JAIRO SILVA TRIVINO <jsilvat2@msuteq.edu.ec>
2026-07-06 16:46:57 -0500

    Initial commit

---
+++ app/src/main/java/com/example/firebaseprac/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.firebaseprac;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+
+    EditText txt_temperatura_edit, txt_humedad_edit, txt_velocidad_edit, txt_presion_edit;
+    DatabaseReference HumedadRef,presionRef,VelocidadRef,TemperauraRef;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        database = FirebaseDatabase.getInstance();
+        HumedadRef =    database.getReference("sensores/humedad");
+
+        presionRef = database.getReference("sensores/presion");
+
+        VelocidadRef =  database.getReference("sensores/velocidad");
+
+        TemperauraRef =     database.getReference("sensores/temperatura");
+
+        TextView txtTemp=findViewById(R.id.valor_Temperatura);
+        TextView txtHum=findViewById(R.id.valor_Humedad);
+        TextView txtVel=findViewById(R.id.valor_Velocidad);
+        TextView txtPres=findViewById(R.id.valor_Presion);
+
+        txt_temperatura_edit = findViewById(R.id.setvalor_Temperatura);
+        txt_humedad_edit = findViewById(R.id.setvalor_Humedad);
+        txt_velocidad_edit = findViewById(R.id.setvalor_Velocidad);
+        txt_presion_edit = findViewById(R.id.setvalor_Presion);
+
+        TemperauraRef.addValueEventListener(setListener(txtTemp, "°C"));
+        HumedadRef.addValueEventListener(setListener(txtHum, "%"));
+        VelocidadRef.addValueEventListener(setListener(txtVel, "km/h"));
+        presionRef.addValueEventListener(setListener(txtPres, "hPa"));
+
+    }
+    public ValueEventListener setListener(TextView txt, String UnidadMedida){
+
+        return (new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.getValue() != null){
+                    txt.setText(snapshot.getValue().toString() + " " + UnidadMedida);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                txt.setText("");
+            }
+        });
+    }
+
+    public void clickBotonTemp(View view){
+        TemperauraRef.setValue(Float.parseFloat(txt_temperatura_edit.getText().toString()));
+    }
+    public void clickBotonHum(View view){
+        HumedadRef.setValue(Float.parseFloat(txt_humedad_edit.getText().toString()));
+    }
+    public void clickBotonVelo(View view){
+        VelocidadRef.setValue(Float.parseFloat(txt_velocidad_edit.getText().toString()));
+    }
+    public void clickBotonPres(View view){
+        presionRef.setValue(Float.parseFloat(txt_presion_edit.getText().toString()));
+    }
+
+}
